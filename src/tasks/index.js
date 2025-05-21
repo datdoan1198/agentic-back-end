e1252e8d270e1b63f20800e5b6d581ec74caa77d
Đoàn Tiến Đạt <datdt@zent.vn>
2025-05-21 10:26:19 +0700

    Update deploy

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -1,8 +1,5 @@
 import scanKnowledgeWeb from './scan-knowledge-web'
-import {APP_ENV, NODE_ENV} from '@/configs'
 
 export default function executeScheduledTasks() {
-    if (NODE_ENV === APP_ENV.PRODUCTION) {
-        scanKnowledgeWeb.start()
-    }
+    scanKnowledgeWeb.start()
 }
